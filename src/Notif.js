50544b358f9556e305366fba01a7a1c56d956e8e
Sakti Dewantoro <saktyd@gmail.com>
2020-10-09 23:20:37 +0700

    create init

---
+++ src/Notif.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Notif.css'
+
+function Notif() {
+    return (
+        <div className="notif">
+            <h1>Notif</h1>
+        </div>
+    )
+}
+
+export default Notif
